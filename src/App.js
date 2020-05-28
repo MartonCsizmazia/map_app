30475d98bb3e0e6a6342a90fc15cccc694856188
MartonCsizmazia <csmarton93@gmail.com>
2020-05-28 23:43:15 +0200

    weather and temperature added to index

--- src/App.js
+++ src/App.js
@@ -19,22 +19,34 @@ function App() {
         let year = d.getFullYear();
 
         return `${day} ${date} ${month} ${year}`
-    }
+    };
 
     return (
     <div className="app warm">
         <main>
-          <div className="search-box">
+            <div className="search-box">
               <input
                   type="text"
                   className="search-bar"
                   placeholder="Search..."
               />
-          </div>
-          <div className="location-box">
+            </div>
+
+            <div className="location-box">
               <div className="location">New York City, US</div>
               <div className="date">{dateBuilder(new Date())}</div>
-          </div>
+            </div>
+
+            <div className="weather-box">
+                <div className="temp">
+                    15°C
+                </div>
+                <div className="weather">
+                    Sunny
+                </div>
+            </div>
+
+
         </main>
     </div>
     );
