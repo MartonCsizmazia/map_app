449654a92c3d140dc41d42a754b944ce9d906505
MartonCsizmazia <csmarton93@gmail.com>
2020-05-28 23:20:36 +0200

    adding backgrounds

--- src/App.js
+++ src/App.js
@@ -2,23 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const api = {
+    key : "730d0ef18b49b5ed854bdb39626c5d51",
+    base: "https://api.openweathermap.org/data/2.5/"
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app warm">
+      <main className="search-box">
+          <input
+              type="text"
+              className="search-bar"
+              placeholder="Search..."
+          />
+      </main>
     </div>
   );
 }
